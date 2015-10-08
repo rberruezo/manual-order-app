0d69e8c6eaf26f689689669dbd3229e45f838d0f
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-10-08 12:12:20 -0300

    Modified page title

--- src/components/main.js
+++ src/components/main.js
@@ -5,8 +5,8 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
-        <Link to='login'>Go to the Login page...</Link>
+        <h1>Manual Order Application</h1>
+        <Link to='login'>Take me to Login page...</Link>
         <RouteHandler/>
       </div>
     );
