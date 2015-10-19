4b7e7395a8e265e69a5652d2661d91e68ecd6d81
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-10-19 15:31:17 -0300

    Modified: user logout calls to reauthenticate the app

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,13 @@
-var mockAuthResponse = {
-	user_token: '54636',
-	status: 'OK'
-}
+var math = require('mathjs');
 
 var authService = {
   authenticateApp: function () {
     return new Promise(function (resolve, reject) {
       setTimeout(function () {
+				var mockAuthResponse = {
+					user_token: math.randomInt(10000, 99999),
+					status: 'OK'
+				};
         resolve(mockAuthResponse);
       }, 250);
     });
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,5 @@
+import AuthActions from 'actions/authActions';
+
 var mockData = {
   status: 'OK'
 };
@@ -12,35 +14,47 @@ var userService = {
   loginUser: function (user) {
     return new Promise(function (resolve, reject) {
       setTimeout(function () {
+
+        // TODO: Call to login service with postData
+        var postData = {
+          email: user.email,
+          pass: user.pass,
+          token: userService.getUserToken()
+        }
+
         mockUsers.map(function(mockUser) {
           if (user.email == mockUser.email && user.pass == mockUser.pass) {
-            localStorage.setItem('email', user.email);
+            localStorage.setItem('user_email', user.email);
             resolve({status: 'OK'});
           }
         });
+
         reject("Error: Invalid user or password");
+
       }, 250);
     });
   },
 
   logoutUser: function () {
-    localStorage.removeItem('email');
+    localStorage.removeItem('user_email');
+    localStorage.removeItem('user_token');
+    AuthActions.authenticateApp();
   },
 
   loggedUser: function() {
-    return localStorage.getItem('email');
+    return localStorage.getItem('user_email');
   },
 
   isLoggedIn: function() {
-    return localStorage.getItem('email') != null;
+    return localStorage.getItem('user_email') != null;
   },
 
   setUserToken: function(token) {
-    localStorage.setItem('token', token);
+    localStorage.setItem('user_token', token);
   },
 
   getUserToken: function() {
-    return localStorage.getItem('token');
+    return localStorage.getItem('user_token');
   }
 };
 
