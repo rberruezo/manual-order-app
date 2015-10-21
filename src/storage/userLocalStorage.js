66690bca893755d49c433b018d067362430169d0
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-10-21 14:29:29 -0300

    Removed unnecessary methods of UserLocalStorage

--- src/storage/userLocalStorage.js
+++ src/storage/userLocalStorage.js
@@ -4,32 +4,6 @@ var userLocalStorage = {
     localStorage.removeItem('user');
   },
 
-  getPersistedUserEmail: function() {
-    return userLocalStorage.getPersistedUserAttribute('email');
-  },
-
-  getPersistedUserToken: function() {
-    return userLocalStorage.getPersistedUserAttribute('token');
-  },
-
-  isLoggedIn: function() {
-    return userLocalStorage.getPersistedUserToken() != null;
-  },
-
-  persistUserAttribute: function(attribute, value) {
-    var user = userLocalStorage.getPersistedUser();
-    user[attribute] = value;
-    localStorage.setItem('user', JSON.stringify(user));
-  },
-
-  getPersistedUserAttribute: function(attribute) {
-    var user = userLocalStorage.getPersistedUser();
-    if (user[attribute] == undefined) {
-      return null;
-    }
-    return user[attribute];
-  },
-
   persistUser: function(user) {
     localStorage.setItem('user', JSON.stringify(user));
   },
