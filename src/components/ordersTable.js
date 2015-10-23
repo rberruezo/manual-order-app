9404837ee2960fad83f90dbc1ccdabf68f602ac0
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-10-23 13:31:52 -0300

    Added new data, metadata and format to the OrdersTable

--- src/components/ordersTable.js
+++ src/components/ordersTable.js
@@ -5,6 +5,23 @@ var Griddle = require('griddle-react');
 
 class OrdersTable extends React.Component {
 
+	getColumnsNames() {
+		return ["createdAt", "order_id", "consumer_name", "item_count"];
+	}
+
+	getColumnsMetadata() {
+		return [
+			{"columnName": "createdAt", "order": 4, "locked": false, "visible": true, "displayName": "Created At"},
+		  {"columnName": "order_id", "order": 1, "locked": false, "visible": true, "displayName": "ID"},
+		  {"columnName": "consumer_name", "order": 2, "locked": false, "visible": true, "displayName": "Consumer"},
+		  {"columnName": "item_count", "order": 3, "locked": false, "visible": true, "displayName": "Item Count"}
+		];
+	}
+
+	handleOrderClick(row, evt) {
+    console.log(row.props.data);
+  }
+
   render() {
     return (
     	<div>
@@ -13,7 +30,20 @@ class OrdersTable extends React.Component {
 				<script src="//fb.me/JSXTransformer-0.13.2.js"></script>
 				<script type="text/javascript" src="scripts/griddle.js"></script>
 
-				<Griddle results={Mocks.getFakeData()} tableClassName="table" showFilter={true} showSettings={true} columns={["name", "city", "country"]}/>
+				<Griddle
+					results={Mocks.getFakeData()}
+					tableClassName="table"
+					showFilter={true}
+					showSettings={true}
+					columns={this.getColumnsNames()}
+					columnMetadata={this.getColumnsMetadata()}
+					noDataMessage={"No data could be found."}
+					initialSort="createdAt"
+					initialSortAscending={true}
+					sortDescendingComponent=' v'
+					sortAscendingComponent=' ^'
+					onRowClick={this.handleOrderClick}
+				/>
 
 			</div>
     );
--- src/mocks/mocks.js
+++ src/mocks/mocks.js
@@ -7,16 +7,15 @@ var mockedUsers = [
 ];
 
 var fakeData = [
-	{"id": 0, "name": "Mayer Leonard", "city": "London", "country": "United Kingdom", "favoriteNumber": 7 },
-	{"id": 1, "name": "Victoria Sarappa", "city": "Rosario", "country": "Argentina", "favoriteNumber": 3 },
-	{"id": 2, "name": "Juan Rojo", "city": "New York", "country": "USA", "favoriteNumber": 5 },
-	{"id": 3, "name": "Clara Orchow", "city": "Buenos Aires", "country": "Argentina", "favoriteNumber": 2 },
-	{"id": 4, "name": "Alan Pieroni", "city": "Krakow", "country": "Poland", "favoriteNumber": 1 },
-	{"id": 5, "name": "Ivan Levin", "city": "Madrid", "country": "Spain", "favoriteNumber": 9 },
-	{"id": 6, "name": "Matias Camacho", "city": "Moscow", "country": "Rusia", "favoriteNumber": 4 },
-	{"id": 7, "name": "Marcia Perdomo", "city": "Beijing", "country": "China", "favoriteNumber": 8 },
-	{"id": 8, "name": "Nicolas Vaamonde", "city": "San Fransisco", "country": "USA", "favoriteNumber": 6 },
-	{"id": 9, "name": "Augusto Rodriguez", "city": "Brasilia", "country": "Brazil", "favoriteNumber": 5 }
+	{"createdAt": 1410231218, "order_id": 1, "consumer_name": "Victoria Sarappa", "item_count": 3 },
+	{"createdAt": 1510231105, "order_id": 2, "consumer_name": "Juan Rojo", "item_count": 5 },
+	{"createdAt": 1306231749, "order_id": 3, "consumer_name": "Clara Orchow", "item_count": 2 },
+	{"createdAt": 1508181228, "order_id": 4, "consumer_name": "Alan Pieroni", "item_count": 1 },
+	{"createdAt": 1511051238, "order_id": 5, "consumer_name": "Ivan Levin", "item_count": 9 },
+	{"createdAt": 1503150753, "order_id": 6, "consumer_name": "Matias Camacho", "item_count": 4 },
+	{"createdAt": 1511182302, "order_id": 7, "consumer_name": "Marcia Perdomo", "item_count": 8 },
+	{"createdAt": 1511181818, "order_id": 8, "consumer_name": "Nicolas Vaamonde", "item_count": 6 },
+	{"createdAt": 1511180729, "order_id": 9, "consumer_name": "Augusto Rodriguez", "item_count": 5 }
 ];
 
 var mocks = {
