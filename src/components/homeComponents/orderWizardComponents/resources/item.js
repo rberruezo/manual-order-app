c07f7b57fceb667d476279b6a5871672a13a2d08
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-05 15:51:24 -0300

    Item view: source url as anchor

--- src/components/homeComponents/orderWizardComponents/resources/item.js
+++ src/components/homeComponents/orderWizardComponents/resources/item.js
@@ -8,35 +8,51 @@ class Item extends React.Component {
         <h2>Item</h2>
         <div>
           <label for="quantity">Quantity</label>
-          <div id="quantity" name="quantity" type="number" readonly>{item.quantity}</div>
+          <div id="quantity" name="quantity" type="number" readonly>
+            {item.quantity}
+          </div>
         </div>
         <div>
           <label for="listPriceCents">List Price Cents</label>
-          <div id="listPriceCents" name="listPriceCents" type="number" readonly>{item.listPriceCents}</div>
+          <div id="listPriceCents" name="listPriceCents" type="number" readonly>
+            {item.listPriceCents}
+          </div>
         </div>
         <div>
           <label for="salePriceCents">Sale Price Cents</label>
-          <div id="salePriceCents" name="salePriceCents" type="number" readonly>{item.salePriceCents}</div>
+          <div id="salePriceCents" name="salePriceCents" type="number" readonly>
+            {item.salePriceCents}
+          </div>
         </div>
         <div>
           <label for="sourceUrl">Source Url</label>
-          <div id="sourceUrl" name="sourceUrl" type="text" readonly>{item.sourceUrl}</div>
+          <div id="sourceUrl" name="sourceUrl" type="text" readonly>
+            <a target="_blank" href={item.sourceUrl} name="sourceUrl">{item.sourceUrl}</a>
+          </div>
         </div>
         <div>
           <label for="status">Status</label>
-          <div id="status" name="status" type="number" readonly>{item.status}</div>
+          <div id="status" name="status" type="number" readonly>
+            {item.status}
+          </div>
         </div>
         <div>
           <label for="name">Name</label>
-          <div id="name" name="name" type="text" readonly>{item.name}</div>
+          <div id="name" name="name" type="text" readonly>
+            {item.name}
+          </div>
         </div>
         <div>
           <label for="color">Color</label>
-          <div id="color" name="color" type="text" readonly>{item.color}</div>
+          <div id="color" name="color" type="text" readonly>
+            {item.color}
+          </div>
         </div>
         <div>
           <label for="size">Size</label>
-          <div id="size" name="size" type="text" readonly>{item.size}</div>
+          <div id="size" name="size" type="text" readonly>
+            {item.size}
+          </div>
         </div>
       </div>
     )
