93d28a910aab98975aea5ea4d31a5f6b6036adb2
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-09 17:51:07 -0300

    Added OrderReview Griddle with the Review of all the items of the Order.

--- src/components/homeComponents/steps/general/orderWizardStep.js
+++ src/components/homeComponents/steps/general/orderWizardStep.js
@@ -6,7 +6,8 @@ class OrderWizardStep extends React.Component {
 
   render() {
     if (this.props.step > this.props.order.partners.length) {
-      return <OrderReview callback={this.props.callbacks.closeWizard} />
+      return <OrderReview order={this.props.order}
+                          callback={this.props.callbacks.closeWizard} />
     }
     return <PartnerWizardFlux items={this.props.order.partners[this.props.step-1].items}
                               name={this.props.order.partners[this.props.step-1].name}
--- src/components/homeComponents/steps/orderReview.js
+++ src/components/homeComponents/steps/orderReview.js
@@ -2,14 +2,64 @@ import React from 'react';
 import Buttonpad from 'components/homeComponents/buttonpads/general/buttonpad';
 import {ORDER_REVIEW} from 'constants/stepTitles';
 import {CLOSE} from 'constants/stepButtonLabels';
+import Griddle from 'griddle-react';
 
 class OrderReview extends React.Component {
+  
+	getItemsData() {
+		var itemsData = [];
+		this.props.order.partners.map(function (partner) {
+			partner.items.map(function (item) {
+				itemsData.push({
+					'partner': partner.name,
+					'name': item.name,
+					'quantity': item.quantity,
+					'color': item.color,
+					'size': item.size,
+					'status': item.status
+				});
+			});
+		});
+		return itemsData;
+	}
+
+	getColumnsMetadata() {
+		return [
+			{columnName: "partner", order: 1, locked: false, visible: true, displayName: "Partner"},
+		  {columnName: "name", order: 2, locked: false, visible: true, displayName: "Product"},
+		  {columnName: "quantity", order: 3, locked: false, visible: true, displayName: "Quantity"},
+		  {columnName: "color", order: 4, locked: false, visible: true, displayName: "Color"},
+		  {columnName: "size", order: 5, locked: false, visible: true, displayName: "Size"},
+		  {columnName: "status", order: 6, locked: false, visible: true, displayName: "Status"}
+		];
+	}
+
   render() {
+		console.log('OrderReview');
+		console.log(this.props.order);
     return (
-      <div className="simple-form">
+    	<div>
         <h2>{ORDER_REVIEW}</h2>
-        <Buttonpad buttons={[{callback: this.props.callback, text: CLOSE}]} />
-      </div>
+	    	<div>
+					<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js"></script>
+					<script src="//fb.me/react-0.13.2.js"></script>
+					<script src="//fb.me/JSXTransformer-0.13.2.js"></script>
+					<script type="text/javascript" src="scripts/griddle.js"></script>
+					<Griddle
+						results={this.getItemsData()}
+						tableClassName="table"
+						showFilter={false}
+						showSettings={false}
+						columnMetadata={this.getColumnsMetadata()}
+						noDataMessage={"No data could be found."}
+						sortDescendingComponent=' v'
+						sortAscendingComponent=' ^'
+					/>
+				</div>
+	      <div className="simple-form">
+	        <Buttonpad buttons={[{callback: this.props.callback, text: CLOSE}]} />
+	      </div>
+	    </div>
     )
   }
 }
