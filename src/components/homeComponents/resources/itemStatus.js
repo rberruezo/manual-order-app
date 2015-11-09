c2660138992a6f6f8dc9ba7f0b9483cc3c3e6a50
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-09 19:04:56 -0300

    Added StatusDescription to show the description of the status instead of the number on the Item step.

--- src/components/homeComponents/resources/itemStatus.js
+++ src/components/homeComponents/resources/itemStatus.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import RadioGroup from 'react-radio-group';
-import {ITEM_OUT_OF_STOCK, ORDER_ITEM_PROCESSED_SUCCESSFULLY, ORDER_ITEM_NOT_PROCESSED_DUE_ORDER_FAILURE, ORDER_ITEM_NOT_PROCESSED_DUE_SITE_FAILURE} from 'constants/orderStatus';
-
+import {ITEM_OUT_OF_STOCK, ITEM_OUT_OF_STOCK_TITLE} from 'constants/orderStatus';
+import {ORDER_ITEM_PROCESSED_SUCCESSFULLY, ORDER_ITEM_PROCESSED_SUCCESSFULLY_TITLE} from 'constants/orderStatus';
+import {ORDER_ITEM_NOT_PROCESSED_DUE_ORDER_FAILURE, ORDER_ITEM_NOT_PROCESSED_DUE_ORDER_FAILURE_TITLE} from 'constants/orderStatus';
+import {ORDER_ITEM_NOT_PROCESSED_DUE_SITE_FAILURE, ORDER_ITEM_NOT_PROCESSED_DUE_SITE_FAILURE_TITLE} from 'constants/orderStatus';
 
 class ItemStatus extends React.Component {
 
@@ -31,10 +33,10 @@ class ItemStatus extends React.Component {
 				<RadioGroup name="selectedValue" selectedValue={this.state.selectedValue} onChange={this.handleChange}>
 				{Radio => (
 					<div>
-						<Radio value={ITEM_OUT_OF_STOCK} />Item out of Stock
-						<Radio value={ORDER_ITEM_PROCESSED_SUCCESSFULLY} />Item processed succesfully
-						<Radio value={ORDER_ITEM_NOT_PROCESSED_DUE_ORDER_FAILURE} />Not processed due Order Failure
-						<Radio value={ORDER_ITEM_NOT_PROCESSED_DUE_SITE_FAILURE} />Not processed due Site Failure
+						<Radio value={ITEM_OUT_OF_STOCK} />{ITEM_OUT_OF_STOCK_TITLE}
+						<Radio value={ORDER_ITEM_PROCESSED_SUCCESSFULLY} />{ORDER_ITEM_PROCESSED_SUCCESSFULLY_TITLE}
+						<Radio value={ORDER_ITEM_NOT_PROCESSED_DUE_ORDER_FAILURE} />{ORDER_ITEM_NOT_PROCESSED_DUE_ORDER_FAILURE_TITLE}
+						<Radio value={ORDER_ITEM_NOT_PROCESSED_DUE_SITE_FAILURE} />{ORDER_ITEM_NOT_PROCESSED_DUE_SITE_FAILURE_TITLE}
 					</div>
 				)}
 				</RadioGroup>
