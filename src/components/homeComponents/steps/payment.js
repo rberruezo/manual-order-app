c3215ef87335cdf0f1546f289c9b7f7b18ccf4ad
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-16 16:37:36 -0300

    Applied new wizard style for Payment view.

--- src/components/homeComponents/steps/payment.js
+++ src/components/homeComponents/steps/payment.js
@@ -5,44 +5,48 @@ class Payment extends React.Component {
   render() {
     var payment = this.props.payment;
     return (
-      <div className="simple-form">
+      <div>
         <h2>{PAYMENT}</h2>
-        <div>
-          <label>Type:</label>
-          <div id="type" name="type" type="text" readOnly>
-            {payment.type}
-          </div>
-        </div>
-        <div>
-          <label>Number:</label>
-          <div id="number" name="number" type="text" readOnly>
-            {payment.number}
-          </div>
-        </div>
-        <div>
-          <label>Expiration Month:</label>
-          <div id="expirationMonth" name="expirationMonth" type="number" readOnly>
-            {payment.expirationMonth}
-          </div>
-        </div>
-        <div>
-          <label>Expiration Year:</label>
-          <div id="expirationYear" name="expirationYear" type="number" readOnly>
-            {payment.expirationYear}
-          </div>
-        </div>
-        <div>
-          <label>Name:</label>
-          <div id="name" name="name" type="text" readOnly>
-            {payment.name}
-          </div>
-        </div>
-        <div>
-          <label>CVV:</label>
-          <div id="cvv" name="cvv" type="text" readOnly>
-            {payment.cvv}
-          </div>
-        </div>
+        <table className='wizard-table'>
+          <tbody className='wizard-tbody'>      
+            <tr className='wizard-tr'>
+              <td> <strong className='book-title'>Type</strong> </td>
+              <td>
+                {payment.type}
+              </td>
+            </tr>
+            <tr className='wizard-tr'>
+              <td> <strong className='book-title'>Number</strong> </td>
+              <td>
+                {payment.number}
+              </td>
+            </tr>
+            <tr className='wizard-tr'>
+              <td> <strong className='book-title'>Expiration Month</strong> </td>
+              <td>
+                {payment.expirationMonth}
+              </td>
+            </tr>
+            <tr className='wizard-tr'>
+              <td> <strong className='book-title'>Expiration Year</strong> </td>
+              <td>
+                {payment.expirationYear}
+              </td>
+            </tr>
+            <tr className='wizard-tr'>
+              <td> <strong className='book-title'>Name</strong> </td>
+              <td>
+                {payment.name}
+              </td>
+            </tr>
+            <tr className='wizard-tr'>
+              <td> <strong className='book-title'>CVV</strong> </td>
+              <td>
+                {payment.cvv}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
