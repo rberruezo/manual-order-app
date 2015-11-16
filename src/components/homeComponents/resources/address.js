925db066393ee7ae4540ad719077e5e5c9e309c6
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-16 16:31:51 -0300

    Applied new wizard style for Shipping & Billing view.

--- src/components/homeComponents/resources/address.js
+++ src/components/homeComponents/resources/address.js
@@ -4,50 +4,52 @@ class Address extends React.Component {
   render() {
     var address = this.props.address;
     return (
-      <div>
-        <div>
-          <label>Address 1:</label>
-          <div id="address1" name="address1" type="text" readOnly>
-            {address.address1}
-          </div>
-        </div>
-        <div>
-          <label>Address 2:</label>
-          <div id="address2" name="address2" type="text" readOnly>
-            {address.address2}
-          </div>
-        </div>
-        <div>
-          <label>City:</label>
-          <div id="city" name="city" type="text" readOnly>
-            {address.city}
-          </div>
-        </div>
-        <div>
-          <label>State:</label>
-          <div id="state" name="state" type="text" readOnly>
-            {address.state}
-          </div>
-        </div>
-        <div>
-          <label>Zip:</label>
-          <div id="zip" name="zip" type="text" readOnly>
-            {address.zip}
-          </div>
-        </div>
-        <div>
-          <label>Status:</label>
-          <div id="status" name="status" type="number" readOnly>
-            {address.status}
-          </div>
-        </div>
-        <div>
-          <label>Address Type:</label>
-          <div id="addressType" name="addressType" type="number" readOnly>
-            {address.addressType}
-          </div>
-        </div>
-      </div>
+      <table className='wizard-table'>
+        <tbody className='wizard-tbody'>      
+          <tr className='wizard-tr'>
+            <td> <strong className='book-title'>Address 1:</strong> </td>
+            <td>
+              {address.address1}
+            </td>
+          </tr>
+          <tr className='wizard-tr'>
+            <td> <strong className='book-title'>Address 2:</strong> </td>
+            <td>
+              {address.address2}
+            </td>
+          </tr>
+          <tr className='wizard-tr'>
+            <td> <strong className='book-title'>City:</strong> </td>
+            <td>
+              {address.city}
+            </td>
+          </tr>
+          <tr className='wizard-tr'>
+            <td> <strong className='book-title'>State:</strong> </td>
+            <td>
+              {address.state}
+            </td>
+          </tr>
+          <tr className='wizard-tr'>
+            <td> <strong className='book-title'>Zip:</strong> </td>
+            <td>
+              {address.zip}
+            </td>
+          </tr>
+          <tr className='wizard-tr'>
+            <td> <strong className='book-title'>Status:</strong> </td>
+            <td>
+              {address.status}
+            </td>
+          </tr>
+          <tr className='wizard-tr'>
+            <td> <strong className='book-title'>Address Type:</strong> </td>
+            <td>
+              {address.addressType}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     )
   }
 }
