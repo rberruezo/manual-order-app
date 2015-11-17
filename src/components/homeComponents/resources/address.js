161152f97f7bbe2ce140c69b1417bbaadc428796
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-17 14:56:07 -0300

    Adapted address view to flexbox.

--- src/components/homeComponents/resources/address.js
+++ src/components/homeComponents/resources/address.js
@@ -1,55 +1,74 @@
 import React from 'react';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
 class Address extends React.Component {
   render() {
     var address = this.props.address;
     return (
-      <table className='wizard-table'>
-        <tbody className='wizard-tbody'>      
-          <tr className='wizard-tr'>
-            <td> <strong className='label-title'>Address 1:</strong> </td>
-            <td>
-              {address.address1}
-            </td>
-          </tr>
-          <tr className='wizard-tr'>
-            <td> <strong className='label-title'>Address 2:</strong> </td>
-            <td>
-              {address.address2}
-            </td>
-          </tr>
-          <tr className='wizard-tr'>
-            <td> <strong className='label-title'>City:</strong> </td>
-            <td>
-              {address.city}
-            </td>
-          </tr>
-          <tr className='wizard-tr'>
-            <td> <strong className='label-title'>State:</strong> </td>
-            <td>
-              {address.state}
-            </td>
-          </tr>
-          <tr className='wizard-tr'>
-            <td> <strong className='label-title'>Zip:</strong> </td>
-            <td>
-              {address.zip}
-            </td>
-          </tr>
-          <tr className='wizard-tr'>
-            <td> <strong className='label-title'>Status:</strong> </td>
-            <td>
-              {address.status}
-            </td>
-          </tr>
-          <tr className='wizard-tr'>
-            <td> <strong className='label-title'>Address Type:</strong> </td>
-            <td>
-              {address.addressType}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className='wizard-table'>
+        <Grid className='wizard-tbody'>
+          <Row>
+            <Col md={7} className='col-md-offset-1'>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Address 1:</strong>
+                </Col>
+                <Col md={8}>
+                  {address.address1}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Address 2:</strong>
+                </Col>
+                <Col md={8}>
+                  {address.address2}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>City:</strong>
+                </Col>
+                <Col md={8}>
+                  {address.city}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>State:</strong>
+                </Col>
+                <Col md={8}>
+                  {address.state}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Zip:</strong>
+                </Col>
+                <Col md={8}>
+                  {address.zip}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Status:</strong>
+                </Col>
+                <Col md={8}>
+                  {address.status}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Address Type:</strong>
+                </Col>
+                <Col md={8}>
+                  {address.addressType}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     )
   }
 }
