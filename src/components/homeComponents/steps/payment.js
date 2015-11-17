f8410e07408a3402beaf181c18d20c260bf5e158
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-17 15:21:49 -0300

    Adapted payment view to flexbox.

--- src/components/homeComponents/steps/payment.js
+++ src/components/homeComponents/steps/payment.js
@@ -1,52 +1,67 @@
 import React from 'react';
 import {PAYMENT} from 'constants/stepTitles';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
 class Payment extends React.Component {
   render() {
     var payment = this.props.payment;
     return (
-      <div>
+      <div className='wizard-table'>
         <h3>{PAYMENT}</h3>
-        <table className='wizard-table'>
-          <tbody className='wizard-tbody'>      
-            <tr className='wizard-tr'>
-              <td> <strong className='label-title'>Type</strong> </td>
-              <td>
-                {payment.type}
-              </td>
-            </tr>
-            <tr className='wizard-tr'>
-              <td> <strong className='label-title'>Number</strong> </td>
-              <td>
-                {payment.number}
-              </td>
-            </tr>
-            <tr className='wizard-tr'>
-              <td> <strong className='label-title'>Expiration Month</strong> </td>
-              <td>
-                {payment.expirationMonth}
-              </td>
-            </tr>
-            <tr className='wizard-tr'>
-              <td> <strong className='label-title'>Expiration Year</strong> </td>
-              <td>
-                {payment.expirationYear}
-              </td>
-            </tr>
-            <tr className='wizard-tr'>
-              <td> <strong className='label-title'>Name</strong> </td>
-              <td>
-                {payment.name}
-              </td>
-            </tr>
-            <tr className='wizard-tr'>
-              <td> <strong className='label-title'>CVV</strong> </td>
-              <td>
-                {payment.cvv}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <Grid className='wizard-tbody'>
+          <Row>
+            <Col md={7} className='col-md-offset-1'>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Type</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.type}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Number</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.number}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Expiration Month</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.expirationMonth}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Expiration Year</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.expirationYear}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>Name</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.name}
+                </Col>
+              </Row>
+              <Row className='wizard-tr'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='label-title'>CVV</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.cvv}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
