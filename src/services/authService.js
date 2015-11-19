c9d57665629d5f4beec81738992a7cede4783a5c
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-19 14:49:55 -0300

    Added user_token from the header response on Login API Call. Removed Mock login call.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,4 @@
 import {API_BASE_URL, API_SIGN_IN_URN} from 'constants/api';
-import Mocks from 'mocks/mocks';
 import Utilities from 'utilities/utilities';
 import request from 'superagent';
 
@@ -11,15 +10,12 @@ var authService = {
 				.post(Utilities.produceUri(API_BASE_URL, API_SIGN_IN_URN))
   			.send(requestData)
 				.end(function (err, res) {
-					var response = JSON.parse(res.text);
 					if (err === null) {
 						delete requestData.app_token;
-						console.log(res);
-						/* MOCKED */
-						var mockRes = Mocks.login(requestData);
-						response.user_token = mockRes.user_token;
-						response.status = mockRes.status;
-						/**********/
+						var response = {
+							email: JSON.parse(res.text).email,
+							user_token: res.header['x-auth-token']
+						}
 	          resolve(response);
 					} else {
 						reject(response.error);
