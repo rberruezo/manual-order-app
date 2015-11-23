4c90824e37a0dbc56dc5785fb6140b19e8faa1eb
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-23 17:43:14 -0300

    Connect OrderWizardStep to OrderWizardActions

--- src/components/homeComponents/fluxes/orderWizardFlux.js
+++ src/components/homeComponents/fluxes/orderWizardFlux.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import WizardFlux from 'components/homeComponents/fluxes/wizardFlux';
-import OrderWizardActions from 'actions/orderWizardActions';
-import OrdersActions from 'actions/ordersActions';
 import OrderWizardStep from 'components/homeComponents/steps/general/orderWizardStep';
 import connectToStores from 'alt/utils/connectToStores';
 import OrderWizardStore from 'stores/orderWizardStore';
-import Utilities from 'utilities/utilities';
 import {Grid, Row, Col} from 'react-flexbox-grid';
-import {SUBMITING} from 'constants/apiCallStatus';
 
 @connectToStores
 class OrderWizardFlux extends WizardFlux {
@@ -23,27 +19,13 @@ class OrderWizardFlux extends WizardFlux {
     return OrderWizardStore.getState();
   }
 
-  getButtonpadCallbacks() {
-    return {
-      previousStep: OrderWizardActions.previousStep,
-      nextStep: OrderWizardActions.nextStep,
-      closeWizard: OrderWizardActions.closeWizard,
-      cancelChanges: OrderWizardActions.cancelChanges,
-      submitOrderStatus: this.submitOrderStatus
-    }
-  }
-
-  submitOrderStatus = evt => {
-    OrderWizardActions.submitStatus(this.props.order);
-  }
-
   render() {
     return (
       <Grid>
         <Row className='center-sm center-md'>
           <h1>Order Wizard</h1>
         </Row>
-        <OrderWizardStep callbacks={this.getButtonpadCallbacks()} />
+        <OrderWizardStep />
       </Grid>
     )
   }
--- src/components/homeComponents/steps/general/orderWizardStep.js
+++ src/components/homeComponents/steps/general/orderWizardStep.js
@@ -5,8 +5,8 @@ import OrderWizardResult from 'components/homeComponents/steps/orderWizardResult
 import Buttonpad from 'components/homeComponents/resources/buttonpad';
 import connectToStores from 'alt/utils/connectToStores';
 import OrderWizardStore from 'stores/orderWizardStore';
+import OrderWizardActions from 'actions/orderWizardActions';
 import {SUBMIT_ORDER_STATUS} from 'constants/stepButtonLabels';
-import {NONE, SUCCESS, FAIL} from 'constants/apiCallStatus';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 
 @connectToStores
@@ -23,6 +23,17 @@ class OrderWizardStep extends React.Component {
     return OrderWizardStore.getState();
   }
 
+  getButtonpadCallbacks() {
+    var _that = this;
+    return {
+      previousStep: OrderWizardActions.previousStep,
+      nextStep: OrderWizardActions.nextStep,
+      closeWizard: OrderWizardActions.closeWizard,
+      cancelChanges: OrderWizardActions.cancelChanges,
+      submitOrderStatus: OrderWizardActions.submitStatus.bind(OrderWizardActions, _that.props.order)
+    }
+  }
+
   render() {
     switch(this.props.step-this.props.order.partners.length) {
       case 1:
@@ -30,7 +41,7 @@ class OrderWizardStep extends React.Component {
           <div>
             <OrderReview />
             <Buttonpad buttons={[
-              {callback: this.props.callbacks.submitOrderStatus, text: SUBMIT_ORDER_STATUS}
+              {callback: this.getButtonpadCallbacks().submitOrderStatus, text: SUBMIT_ORDER_STATUS}
             ]} />
           </div>
           )
@@ -38,12 +49,12 @@ class OrderWizardStep extends React.Component {
         return (
           <Row className='center-md'>
             <Col>
-              <OrderWizardResult callbacks={this.props.callbacks} />
+              <OrderWizardResult callbacks={this.getButtonpadCallbacks()} />
             </Col>
           </Row>
           )
       default:
-        return <PartnerWizardFlux callbacks={this.props.callbacks} />
+        return <PartnerWizardFlux callbacks={this.getButtonpadCallbacks()} />
     }
   }
 
