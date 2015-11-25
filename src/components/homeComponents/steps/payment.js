2e1eb6d61e0ac08ae0f4608f02a8ed234fe3e092
Ramiro Berruezo <ramiro.berruezo@graion.com>
2015-11-25 17:50:28 -0300

    Applied new UI to Payment

--- src/components/homeComponents/steps/payment.js
+++ src/components/homeComponents/steps/payment.js
@@ -31,69 +31,69 @@ class Payment extends React.Component {
   render() {
     var payment = this.props.orderWizard.order.paymentData;
     return (
-      <div>
-        <div className='wizard-table'>
+      <section className="wizard-subbox">
+        <header className="wizard-subheader">
           <h3>{PAYMENT}</h3>
-          <Grid className='wizard-table-body'>
-            <Row>
-              <Col md={8} className='col-md-offset-1'>
-                <Row className='wizard-table-row'>
-                  <Col md={2} className='col-md-offset-1'>
-                    <strong className='wizard-label-title'>Type</strong>
-                  </Col>
-                  <Col md={8}>
-                    {payment.type}
-                  </Col>
-                </Row>
-                <Row className='wizard-table-row'>
-                  <Col md={2} className='col-md-offset-1'>
-                    <strong className='wizard-label-title'>Number</strong>
-                  </Col>
-                  <Col md={8}>
-                    {payment.number}
-                  </Col>
-                </Row>
-                <Row className='wizard-table-row'>
-                  <Col md={2} className='col-md-offset-1'>
-                    <strong className='wizard-label-title'>Expiration Month</strong>
-                  </Col>
-                  <Col md={8}>
-                    {payment.expirationMonth}
-                  </Col>
-                </Row>
-                <Row className='wizard-table-row'>
-                  <Col md={2} className='col-md-offset-1'>
-                    <strong className='wizard-label-title'>Expiration Year</strong>
-                  </Col>
-                  <Col md={8}>
-                    {payment.expirationYear}
-                  </Col>
-                </Row>
-                <Row className='wizard-table-row'>
-                  <Col md={2} className='col-md-offset-1'>
-                    <strong className='wizard-label-title'>Name</strong>
-                  </Col>
-                  <Col md={8}>
-                    {payment.name}
-                  </Col>
-                </Row>
-                <Row className='wizard-table-row'>
-                  <Col md={2} className='col-md-offset-1'>
-                    <strong className='wizard-label-title'>CVV</strong>
-                  </Col>
-                  <Col md={8}>
-                    {payment.cvv}
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Grid>
-        </div>
+        </header>
+        <Grid className='wizard-content'>
+          <Row>
+            <Col md={8} className='col-md-offset-1'>
+              <Row className='wizard-table-row'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='wizard-label-title'>Type</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.type}
+                </Col>
+              </Row>
+              <Row className='wizard-table-row'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='wizard-label-title'>Number</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.number}
+                </Col>
+              </Row>
+              <Row className='wizard-table-row'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='wizard-label-title'>Expiration Month</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.expirationMonth}
+                </Col>
+              </Row>
+              <Row className='wizard-table-row'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='wizard-label-title'>Expiration Year</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.expirationYear}
+                </Col>
+              </Row>
+              <Row className='wizard-table-row'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='wizard-label-title'>Name</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.name}
+                </Col>
+              </Row>
+              <Row className='wizard-table-row'>
+                <Col md={2} className='col-md-offset-1'>
+                  <strong className='wizard-label-title'>CVV</strong>
+                </Col>
+                <Col md={8}>
+                  {payment.cvv}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
         <Buttonpad buttons={[
                     {callback: PartnerWizardActions.previousStep, text: BACK},
                     {callback: PartnerWizardActions.submitStatus.bind(PartnerWizardActions, this.getPartner().items), text: SUBMIT_ITEM_STATUS}
                   ]} />
-      </div>
+      </section>
     )
   }
 }
